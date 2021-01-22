a545d83f49de5ef4d1eb90ef8def1e1a7822dfc1
sciences4life100 <70729415+sciences4life100@users.noreply.github.com>
2021-01-21 16:49:05 -0800

    Create ChelseyTalla.java

---
+++ ChelseyTalla.java
@@ -0,0 +1,26 @@
+public class ChelseyTalla {
+    public static void main(String[] args) {
+        System.out.println("Personal Profile of Chelsey Talla");
+        System.out.println("I am born in Kamloops, but I am Hindu. Most people think I am from India, but that is because my parents are from there. That is why I may look Indian, but I refer to myself as Indocanadian!");
+        System.out.println("I am taking the Bachelor of Science program at TRU and this is my second semester. At first I chose this program because I wanted to persue my career in Med, but after a while I found myself very interested with tech and programming. Therefore, I decided to try this course out to see if I should continue my career in Computer Sciences instead.");
+        System.out.println("When I am not studying or in classes, I like to listen to Kpop music and watch Asian dramas and reality shows, I could binge watch if I had to! One of my favourite Kpop group is BTS, I am a huge fan of them and their music is really inspiring! They have a huge impact on my life! I suggest checking out their music. Other than music and shows I like to draw, cook, bake, play games and talk to others online. I love exploring and learning new things.");
+        System.out.println("These are my initials:");                          
+        System.out.println("   ____ _____ ");
+        System.out.println("  / ___|_   _|");
+        System.out.println(" | |     | |  ");
+        System.out.println(" | |___  | |  ");
+        System.out.println("  \\____| |_|  "); 
+        System.out.println("This is my favourite Kpop Group and my bias:");
+        System.out.println(" ____ _____ ____            _____  _    _____ _   ___   ___   _ _   _  ____ ");
+        System.out.println("| __ )_   _/ ___|          |_   _|/ \\  | ____| | | \\ \\ / / | | | \\ | |/ ___| ");
+        System.out.println("|  _ \\ | | \\___ \\   _____    | | / _ \\ |  _| | |_| |\\ V /| | | |  \\| | |  _  ");
+        System.out.println("| |_) || |  ___) | |_____|   | |/ ___ \\| |___|  _  | | | | |_| | |\\  | |_| | ");
+        System.out.println("|____/ |_| |____/            |_/_/   \\_\\_____|_| |_| |_|  \\___/|_| \\_|\\____| ");
+        System.out.println("Lastly, this is my message to everyone:");
+        System.out.println("___       ___________   _______________________   _____________  _______________________________");
+        System.out.println("__ |     / /__  ____/   __  ____/_  __ \\__  __/   ___  __/__  / / /___  _/_  ___/__  /__  /__  /");
+        System.out.println("__ | /| / /__  __/      _  / __ _  / / /_  /      __  /  __  /_/ / __  / _____ \\__  /__  /__  / ");
+        System.out.println("__ |/ |/ / _  /___      / /_/ / / /_/ /_  /       _  /   _  __  / __/ /  ____/ / /_/  /_/  /_/  ");
+        System.out.println("____/|__/  /_____/      \\____/  \\____/ /_/        /_/    /_/ /_/  /___/  /____/ (_)  (_)  (_)   ");
+    }
+}
