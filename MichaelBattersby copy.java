de7b6d7c64f316c24c7e14046cd7606678628362
michaelB-comp <77687101+michaelB-comp@users.noreply.github.com>
2021-01-23 13:27:34 -0400

    Create MichaelBattersby copy.java

---
+++ MichaelBattersby copy.java
@@ -0,0 +1,18 @@
+public class MichaelBattersby {
+    public static void main (String[] args)
+    {
+        System.out.println("My name is Michael Battersby and I am in the Bachelor of Computer Science Program.");
+        System.out.println("");
+
+        System.out.println("I have four German Shepherd dogs and  I support Chelsea Football Club .");
+        System.out.println("");
+
+        System.out.println("My initials are : ");
+          System.out.println("        ________         _______                 ____________");
+          System.out.println("       /       \\        /       \\               |            \\");
+          System.out.println("      /         \\      /         \\              |____________ \\");
+          System.out.println("     /           \\    /           \\             |             /");
+          System.out.println("    /             \\  /             \\            |            / ");
+          System.out.println("   /_______________\\/ ______________\\           |___________/");
+    }
+}
