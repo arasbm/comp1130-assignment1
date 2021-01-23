ec2b19cc4cbc94d1a6f233079bdf889f7d670ac4
juliaa02 <77691795+juliaa02@users.noreply.github.com>
2021-01-22 16:33:08 -0800

    Create JuliaPoole.java

---
+++ JuliaPoole.java
@@ -0,0 +1,21 @@
+public class JuliaPoole{
+public static void main(String[] args){
+    System.out.println("Hello my name is Julia.");
+    System.out.println("I was born in Quesnel B.C. and lived there my whole life until I moved to Kamloops to attend TRU in 2019.");
+    System.out.println("I am currently in my second year of the Bachelor of Science program, and am planning to major in biology.");
+    System.out.println("In my spare time I love being outdoors, I enjoy skiing, biking, and going for walks in nature.");
+    System.out.println("My initials are:");
+    System.out.println("      _ ____  ");
+    System.out.println("     | |  _ \\ ");
+    System.out.println("  _  | | |_) |");
+    System.out.println(" | |_| |  __/ ");
+    System.out.println("  \\___/|_|");
+        }
+    }
+
+
+
+
+
+
+
