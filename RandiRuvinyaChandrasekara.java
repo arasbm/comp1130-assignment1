18568986797161220fb3a43958e434b930fb4307
randicsl <77735912+randicsl@users.noreply.github.com>
2021-01-23 12:21:25 -0800

    Revert "Revert "Create RandiRuvinyaChandrasekara.java""
    This reverts commit 910d02e0f5bfadb65d23b48a6e42249f251897fe.

---
+++ RandiRuvinyaChandrasekara.java
@@ -0,0 +1,16 @@
+public class RandiRuvinyaChandrasekara {
+    public static void main(String[] args) {
+        System.out.println("Personal Profile of Randi Ruvinya Chandrasekara Chandrasekara Mudiyanselage");
+        System.out.println("I am from Sri Lanka, but currently I am in Kamloops");
+        System.out.println("I am pursuing the Bachelors degree at TRU");
+        System.out.println("I love pets and I have a little puppy in my home");
+        System.out.println("My favourite leisure time activities are listening classical music, Bakings and chatting with my loved ones");
+        System.out.println("I am a very family attached child and I really miss my parents during my stay in Kamloops");
+        System.out.println("These are my initials:");
+        System.out.println(" ____  ____   ____ ____ __  __ ");
+        System.out.println("|  _ \\|  _ \\ / ___/ ___|  \\/  |");
+        System.out.println("| |_) | |_) | |  | |   | |\\/| |");
+        System.out.println("|  _ <|  _ <| |__| |___| |  | |");
+        System.out.println("|_| \\_\\_| \\_\\\\____\\____|_|  |_|");
+    }
+}
