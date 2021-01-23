b9336019991bebdeeb8aa12b4cb2ef42416fef86
RhayaDykstra <77759039+RhayaDykstra@users.noreply.github.com>
2021-01-22 18:48:39 -0800

    Create Rhaya.java

---
+++ Rhaya.java
@@ -0,0 +1,13 @@
+public class Rhaya{
+    public static void main(String[] args) {
+        System.out.println("Hello my name is Rhaya") ;
+        System.out.println("I was born in the lower mainland and moved to the interior after highschool") ;
+        System.out.println("I like dance, guitar, yoga, horror movies and iced coffees") ;
+        System.out.println(" _____     ____") ;
+        System.out.println("|  _  \\   |  __  \\") ;
+        System.out.println("| |_|  |   | | |  |") ;
+        System.out.println("|  _ <     | |_|  |") ;
+        System.out.println("| | \\ \\  |      | ") ;
+        System.out.println("|_|  \\_\\ |_____/") ;
+    }
+}
