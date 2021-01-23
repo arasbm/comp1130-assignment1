d49de8c4f00d32bf27e4e310dc7a05208a996c62
G-urjit <77689943+G-urjit@users.noreply.github.com>
2021-01-22 17:12:22 -0800

    Completed

---
+++ GurjitKaur.java
@@ -0,0 +1,20 @@
+
+public class GurjitKaur{
+
+    public static void main(String args[]){
+
+            System.out.println("I am from Uganda, doing Bachelors of Science in Math. I like cooking and chocolates.");
+            System.out.println("I did UNEB in Uganda as my Grade 12 after which I applied to come to study in Canada");
+
+            System.out.println(" _________          |    /");
+            System.out.println("/         \\         |   /");
+            System.out.println("|          |        |  /");
+            System.out.println("|                   | /");
+            System.out.println("|                   |/");
+            System.out.println("|       __          |\\");
+            System.out.println("|         \\         | \\");
+            System.out.println("|          |        |  \\");
+            System.out.println("|          |        |   \\");
+            System.out.println("\\_________/         |    \\"); 
+    }
+}
