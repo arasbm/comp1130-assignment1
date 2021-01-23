e293005a6c0788f4d184d82f9f312ebf7feb3af8
Vishrant <vishupatel213@gmail.com>
2021-01-22 20:49:43 -0800

    Assignment completed

---
+++ vishrantPatel.java
@@ -0,0 +1,19 @@
+public class vishrantPatel{
+     public static void main(String[] args) {
+    System.out.println("hello!");
+    System.out.println("my name is Vishrant Patel. I was born in India. I spend most of my free time in reading books, and talking with my parents because right now I am in kamloops." + "my favriout book is \'ANGELS AND DEMONS\' which is a si-fi book" + " written by Dan Brown. Si-fi books are the one which made my intrest in technology and science." );
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+       System.out.println("My initils are :-");
+        System.out.println("=========================================================================================");
+        System.out.println(" __           __    __________      ________               ");
+        System.out.println("\\V \\        /v /   | S_S_S_S__|    |P P_P_P |              ");
+        System.out.println(" \\V \\      /V /    | S|________    |P |___|P|             ");
+        System.out.println("  \\V \\    /V /     |_S_S_S_S S|    |P _P_P_P|      ");
+        System.out.println("   \\V \\__/V /      _________|S|    |P |                 ");
+        System.out.println("    \\V_V_V_/       |_S_S_S_S_S|    |P_|       ");
+        System.out.println("=========================================================================================");
+}
+}
