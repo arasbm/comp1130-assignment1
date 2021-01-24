5ff1ef70c6b2204ac0ba56f8dd2f97fa537a0644
ShivanshJoshi19 <77924846+ShivanshJoshi19@users.noreply.github.com>
2021-01-24 20:49:10 +0530

    Create Shivanshjoshi.java

---
+++ Shivanshjoshi.java
@@ -0,0 +1,22 @@
+public public class Shivanshjoshi
+{
+    public static void main(String[] args)
+    {
+        System.out.println(" PROFILE OF MASTER SHIVANSH JOSHI");
+        System.out.println("My name is Shivansh Joshi, and you can call me Shivi. I am from Yamuna Nagar, which is a city near the river Yamuna in Haryana, India. I am 19 years old");
+        System.out.println(" I have a passion to develop new technology so that it can improve society and can change the world, so to accomplish my passion I procured admission in TRU's BCS program.");
+        System.out.println("I am also a sports enthusiast as I am a state-level player in Cricket and played many inter-school football competitions.");
+        System.out.println(" I am an animal lover and I have a pet dog which is a german shepherd, I spent my free time playing with him.");
+        System.out.println("Here Are My Initials");
+        System.out.println("  ____      _"); 
+        System.out.println(" / ___|    | |");
+        System.out.println(" \\___ \\ _  | |");                     
+        System.out.println("  ___) | |_| |");         
+        System.out.println(" |____/ \\___/");                     
+                                        
+    }
+    
+
+}
+    
+
