dd01b7c3f87b55df236427379132c492022ffaa8
Amrit-Randhawa <77762844+Amrit-Randhawa@users.noreply.github.com>
2021-01-23 20:32:20 -0800

    Create AmritpalSinghRandhawa.java

---
+++ AmritpalSinghRandhawa.java
@@ -0,0 +1,28 @@
+public class AmritpalSinghRandhawa {
+  public static void main(String[] args) {
+  System.out.println("Personal Profile of Amritpal Singh Randhawa"); // display message
+  System.out.println();  // display blank
+  
+  System.out.println("I speak Punjabi and English, I was born in Kamloops BC, but grew up in Merritt BC."); // display message
+  System.out.println();  // display blank
+ 
+  System.out.println("I am in the Computer Science program at TRU. Taking An Introduction to Programming, and this is one of my first Java programs!"); // display message
+  System.out.println();  // display blank
+  
+  System.out.println("When not studying I enjoy playing games, watching movies, and spending time with family."); // display message
+  System.out.println();  // display blank
+  
+  System.out.println("My initials are:"); 
+  System.out.println();  // display blank
+  
+  System.out.println(" AAAAAAAA    SSSSSS     RRRRR RRRR ");
+  System.out.println(" AAAA AAA   S       S   RRRRR RRRR");
+  System.out.println("AAAAAAAAAA  SSSS        RRR    RR");
+  System.out.println("AA      AA      SSSS    RRR RRR:");
+  System.out.println("AA      AA  S        SS RRR   RRR");
+  System.out.println("AA      AA   SSSSS  SS  RRR    RR  RR");
+}
+
+
+
+}
