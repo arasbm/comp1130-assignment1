182aa17f37215174d38d7e3b44f6d6eb429a4ce3
jeanyjsu <77689370+jeanyjsu@users.noreply.github.com>
2021-01-24 05:27:01 -0800

    Create JeanSu.java

---
+++ JeanSu.java
@@ -0,0 +1,15 @@
+public class JeanSu{
+     public static void main(String[] args){
+          System.out.println("Hi, I'm Jean, or call me Ya Ching if you want to be formal. ");
+          System.out.println("I still need to figure out how to change my name to that in class. ");
+          System.out.println("Nothing really interesting about me but my best friend in Taiwan just had her wedding this weekend. ");
+          System.out.println("I'm so happy for her but sad at the same time that I'm unable to attend her wedding due to the pandemic.");
+          System.out.println("Here's my initial:");
+          System.out.println("     ____. _________");
+          System.out.println("    |    |/   _____/");
+          System.out.println("   |    |\\_____  \\ ");
+          System.out.println("/\\__|    |/        \\");
+          System.out.println("\\________/_______  /");
+          System.out.println("                 \\/ ");
+     }
+}
