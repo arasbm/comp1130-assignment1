994ba45e3385a4c143b26ab9a25694fdbe972394
cade411 <cade411@gmail.com>
2021-01-25 10:57:18 -0800

    Create CadeTorjusson.java

---
+++ CadeTorjusson.java
@@ -0,0 +1,14 @@
+public class CadeTorjusson{  
+    public static void main(String[] args) {
+       System.out.println("Assignment 1 for Cade Torjusson");
+       System.out.println("I was born in Canada in 2001 and was initially raised in Kamloops. I moved to Chase when I was 8 and lived there until I started attending university here at TRU.");
+       System.out.println("I just started to learn how to snowboard at the beggining of the winter and it has been my favourite past time since then. I ussually go up to Harper mountain with some friends during the weekend and spend the whole day up there");
+       System.out.println("    __________   ___________        ");
+       System.out.println("   |  ________| |____  _____|       ");
+       System.out.println("   | |               | |            ");
+       System.out.println("   | |               | |            ");
+       System.out.println("   | |               | |        O   ");
+       System.out.println("   | |________       | |       /|\\ ");
+       System.out.println("   |__________|      |_|        /\\ ");
+    }
+      }
