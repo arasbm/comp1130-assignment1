0cbd3eafd934eb2e3ddb65613b1c512be638b224
BH-mia <77725596+BH-mia@users.noreply.github.com>
2021-01-25 21:01:08 +0800

    Create BaihongLi.java

---
+++ BaihongLi.java
@@ -0,0 +1,15 @@
+public class BaihongLi {
+    public static void main(String[] args) {
+        System.out.println("Personal Profile of Baihong Li,");
+        System.out.println("I am a second year student in TRU. My major is Mathematics and Physics.");
+        System.out.println("I am Chinese. Due to some technical issues, my name appears in Chinese characters:李佰鸿 in virtual class");
+        System.out.println("I like to listen to rap, especially melodic rap.");
+        System.out.println("Here are my initials:");
+        System.out.println(" _____");
+        System.out.println("|  _   \\ |");
+        System.out.println("| |_ ) | |");
+        System.out.println("|  _   < |");
+        System.out.println("| |_ ) | |");
+        System.out.println("|_____ / |______");
+    }
+}
