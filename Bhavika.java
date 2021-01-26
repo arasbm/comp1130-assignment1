969b4e804d0a4d805ead445867bd7bf19ae25bca
bhavikaxjain <77770993+bhavikaxjain@users.noreply.github.com>
2021-01-26 22:28:41 +0530

    Create Bhavika.java

---
+++ Bhavika.java
@@ -0,0 +1,13 @@
+public class Bhavika{
+    public static void main ( String[] args) {
+        System.out.println("Hey, I'm Bhavika Jain, 18 years old from India. This is my first semester at TRU.");
+        System.out.println("Talking about my hobbies, I really enjoy cooking and experimenting in the kitchen.");
+        System.out.println("Nevertheless, I also love dancing and like to read books.");
+        System.out.println("My initials are:");
+        System.out.println(" ____      _ ");
+        System.out.println("| __ )    | |");
+        System.out.println("|  _ \\ _  | |");
+        System.out.println("| |_) | |_| |");
+        System.out.println("|____/ \\___/ ");
+    }
+}
