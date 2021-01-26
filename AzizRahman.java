ef7283e1065fe0069fe66c938098545d211734b8
St-Edik <azizr1462@gmail.com>
2021-01-25 18:41:53 -0800

    Create AzizRahman.java

---
+++ AzizRahman.java
@@ -0,0 +1,15 @@
+public class AzizRahman {
+    public static void main(String[] args) {
+        System.out.println("Personal Profile of Aziz Rahman");
+        System.out.println("I was born in Bangladesh and came into Kamloops last year.");
+        System.out.println("I am currently on the second semester of my Bachelor of Science on Computing Science.");
+        System.out.println("I don't really have a specific genre of music that I like, if the music or the song sounds good, resonates with me or if I even like the tune, I listen to it.");
+        System.out.println("When I'm on my own time, I take walks, try to cover new songs and wonder about various aspects of life.");
+        System.out.println("My initials are:");
+        System.out.println("     _    ____  ____  ");
+        System.out.println("    / \\  |  _ \\/ ___| ");
+        System.out.println("   / _ \\ | |_) \\___ \\ ");
+        System.out.println("  / ___ \\|  _ < ___) |");
+        System.out.println(" /_/   \\_\\_| \\_\\____/ ");
+    }
+}
