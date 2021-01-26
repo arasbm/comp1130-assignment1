203a7636a4e63a38032d65db7e0a12d538dbb7ef
kimliad1432 <50343491+kimliad1432@users.noreply.github.com>
2021-01-25 23:16:44 -0800

    Create DenisKimlia.java

---
+++ DenisKimlia.java
@@ -0,0 +1,29 @@
+public class DenisKimlia{
+
+    public static void main(String[] args){
+
+        System.out.println();
+        System.out.println("Hello! This is personal profile of Denis Kimlia,");
+        System.out.println();
+        System.out.println("I was born in Russia. I am 18 years old and now mooved to Kamloops.");
+        System.out.println();
+        System.out.println("I like video games, programming and basketball. When i am not studyin or"); 
+        System.out.println();
+        System.out.println("playing video games and the weather is fine i like to spend my free time");
+        System.out.println();
+        System.out.println("at basketball court or just walking.");
+        System.out.println();
+
+
+        System.out.println("Here are my initials: ");
+
+        System.out.println(" _____    __   ___               ");
+        System.out.println("|     \\  |  | /  /             ");
+        System.out.println("|  |\\  \\ |  |/  /              ");
+        System.out.println("|  | \\  \\|   _  \\                ");
+        System.out.println("|  | /  /|  | \\  \\               ");
+        System.out.println("|  |/  / |  |  \\  \\           ");
+        System.out.println("|_____/  |__|   \\__\\            "); 
+        
+    }
+}
