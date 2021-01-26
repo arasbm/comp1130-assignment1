5bd6d0bd71125838d19a1e4ed405c5d3436aa781
KseniaBoyko <78051257+KseniaBoyko@users.noreply.github.com>
2021-01-26 15:10:46 -0800

    Add files via upload

---
+++ KseniaBoyko.java
@@ -0,0 +1,8 @@
+public class KseniaBoyko
+{
+ public static void main(String[] args)
+ {
+ System.out.println("Currently, I am trying hard to understand programming.\nUnfortunately, I have some health issues that came up recently, but I hope to get better soon.\nReally dream about a vacation!");
+ System.out.println("\n _     ___  \n| |_/ | |_) \n|_| |.|_|_).");
+ }
+}
