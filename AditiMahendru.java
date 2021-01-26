a41c4958c275ce7ec2356df5bee2c558f4247f3b
aditimahendru <77691060+aditimahendru@users.noreply.github.com>
2021-01-25 23:54:56 -0800

    Create AditiMahendru.java

---
+++ AditiMahendru.java
@@ -0,0 +1,14 @@
+public class AditiMahendru {
+    public static void main(String[] args) {
+        System.out.println("Personal profile of Aditi Mahendru");
+        System.out.println("I am born and raised in India and I am a Hindu");
+        System.out.println("I am currently living in Kamloops and I have been doing my Bachelors in Science and Chemical Biology as my major since last winter as it is my second year at TRU.");
+        System.out.println("During my spare time, I like watching SitComs and decorating my dorm");
+        System.out.println("These are my initials:");
+        System.out.println("     _    __  __ ");
+        System.out.println("    / \\  |  \\/  |"); 
+        System.out.println("   / _ \\ | |\\/| |");
+        System.out.println("  / ___ \\| |  | |");
+        System.out.println(" /_/   \\_\\_|  |_|");
+    }
+}
