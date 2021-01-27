aae1c44957d42b120722ea8ab29cd4d9eee58a3a
andregardner <78049474+andregardner@users.noreply.github.com>
2021-01-26 18:03:55 -0800

    Update Andre.java

--- Andre.java
+++ Andre.java
@@ -5,9 +5,9 @@ public class Andre {
             System.out.println("I am a second year Chemical Biology student.");
             System.out.println("In my free time I enjoy going to the gym, skiing and golfing.");
             System.out.println("My initials are:"); 
-            System.out.println("      __	       ________       ");
+            System.out.println("      __	     ________       ");
             System.out.println("     //\\          //       \\      ");
-            System.out.println("    //  \\	    //                ");
+            System.out.println("    //  \\	  //                ");
             System.out.println("   //    \\      //      ______     ");
             System.out.println("  //______\\     ||      ||   ||    ");
             System.out.println(" //        \\    \\          //     ");
