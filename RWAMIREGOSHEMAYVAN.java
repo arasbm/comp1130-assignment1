8316fdb5079f7b80ca5a91b16d4633c2b05f1528
shemayvan <77689737+shemayvan@users.noreply.github.com>
2021-01-26 19:10:56 -0500

    Create RWAMIREGOSHEMAYVAN.java
    Info on Shema Yvan for assignment

---
+++ RWAMIREGOSHEMAYVAN.java
@@ -0,0 +1,15 @@
+public class RWAMIREGOSHEMAYVAN{
+  public static void main(String[] args){
+    System.out.println("Personal details for Rwamirego Shema Yvan: ");
+    System.out.println();
+    System.out.print("I was born and raised in Rwanda and am majoring in Computer Science. ");
+    System.out.print("I came to TRU because my family moved here and wanted to stay close to them ");
+    System.out.println("for my university studies. However, Due to COVID we now live in Gatineau!");
+    System.out.println();
+    System.out.print("I like to read and watch anime in my spare time and mostly stay indoors.");
+    System.out.println("It is a pleasure taking this course with you guys. I hope we have all fun!");
+    System.out.println("My intials are: RSY ");
+    System.out.println();
+    System.out.print("Note: I am sorry I do not know how to do the ASCII art!");
+  }
+}
