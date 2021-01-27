7f381bdfe2efbd4100930afc310aa6cbebea77aa
aditimahendru <77691060+aditimahendru@users.noreply.github.com>
2021-01-26 16:06:04 -0800

    Create AditiMahendru.java

---
+++ AditiMahendru.java
@@ -0,0 +1,13 @@
+public class AditiMahendru {
+    public static void main(String[] args) {
+    System.out.println("Persnal profile of Aditi Mahendru");
+    System.out.println("I am born and raised in India and I am a Hindu");
+    System.out.println("During my spare time, I like watching SitComs and decorating my dorm");
+    System.out.println("These are my intials:");
+    System.out.println("     _    __  __ ");
+    System.out.println("    / \\  |  \\/  |");
+    System.out.println("   / _ \\ | |\\/| |");
+    System.out.println("  / ___ \\| |  | |");
+    System.out.println(" /_/   \\_\\_|  |_|");
+    }
+}
