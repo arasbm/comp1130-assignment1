06668dd341833767bd1faa822dba09ca340accaf
Robby <78438635+mudogo20r@users.noreply.github.com>
2021-02-23 07:40:19 -0800

    Add files via upload

---
+++ RobertMudogo.java
@@ -0,0 +1,24 @@
+/*
+ *   Copyright (c) 2021 
+ *   All rights reserved.
+ */
+public class RobertMudogo {
+    public static void main(String[] args){
+        System.out.println("I love playing rugby");
+        System.out.println("My favorite dish is anything that is spiced well");
+        System.out.println("I love meeting new people");
+        System.out.println("To me life is like a game that you play to win");
+        System.out.println(" __");
+        System.out.println("|  \\  |\\      /|");
+        System.out.println("|  /  | \\    / |");
+        System.out.println("| /   |  \\  /  |");
+        System.out.println("|/    |   \\/   |");
+        System.out.println("|\\    |        |");
+        System.out.println("| \\   |        |");
+
+
+
+
+
+    }
+}
